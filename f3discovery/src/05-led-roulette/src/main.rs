0cfea959f601cfcd53b874b4275df1494fcad06c
Eduardo Villarreal <evillarreal@ferrodevices.com>
2024-10-25 17:36:24 -0600

    Flashing LEDs WOOO

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, stm32f3xx_hal::hal::blocking::delay, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
+    
+    let ms = 50_u8;
+    
+    loop {
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+            leds[next].on().ok();
+            delay.delay_ms(ms);
+            leds[curr].off().ok();
+            delay.delay_ms(ms);
+        }
+    } 
 }
